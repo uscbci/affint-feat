7954a5de789139942bec963b544e228568e340a2
Jonas Kaplan <jtkaplan@usc.edu>
2020-02-18 13:51:14 -0800

    add prep for mvpa script

---
+++ mvpa_prepare.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+from subprocess import call 
+
+#command line options
+if (len(sys.argv) < 2):
+	print "\n\tusage: %s <subject>\n" % sys.argv[0]
+	sys.exit()
+else:
+	subject = sys.argv[1]	
+
+print("Preparing affective pictures output for MVPA analysis...")
+
+FLYWHEEL_BASE="/flywheel/v0"
+OUTPUT_DIR="%s/output" % FLYWHEEL_BASE
+INPUT_DIR="%s/input" % FLYWHEEL_BASE
+
+
+numcopes = 5
+
+command = "fslmerge -t %s/%s_affectivepictures_all " % (OUTPUT_DIR,subject)
+
+for run in range(1,4):
+	feat_dir = "%s/affectivepictures_run%d.feat" % (OUTPUT_DIR,run)
+	for cope in range(1,numcopes+1):
+		newcope = "%s/stats/zstat%d " % (feat_dir,cope)
+		command = command + newpart
+
+print(command)
+call(command,shell=True)
